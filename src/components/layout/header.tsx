29037e22ad61e71df9a9e6e07b526d3b5783c846
Geovanni Mena <geomenacontact@gmail.com>
2025-06-19 19:57:07 -0500

    ✨ refactor: comment out unused Breadcrumbs import and related code for cleaner header component

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { SidebarTrigger } from '../ui/sidebar';
 import { Separator } from '../ui/separator';
-import { Breadcrumbs } from '../breadcrumbs';
+// import { Breadcrumbs } from '../breadcrumbs';
 import SearchInput from '../search-input';
 import { UserNav } from './user-nav';
 import { ThemeSelector } from '../theme-selector';
@@ -12,8 +12,8 @@ export default function Header() {
         <header className='flex h-16 shrink-0 items-center justify-between gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12'>
             <div className='flex items-center gap-2 px-4'>
                 <SidebarTrigger className='-ml-1' />
-                <Separator orientation='vertical' className='mr-2 h-4' />
-                <Breadcrumbs />
+                {/* <Separator orientation='vertical' className='mr-2 h-4' />
+                <Breadcrumbs /> */}
             </div>
 
             <div className='flex items-center gap-2 px-4'>
