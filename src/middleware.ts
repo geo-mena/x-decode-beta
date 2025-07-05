ded9a602ac1e8022f4b37d671d6fc376b930c473
Geovanni Mena <geomenacontact@gmail.com>
2025-07-05 00:17:41 -0500

    fix: update middleware to use NextResponse for improved request handling

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,19 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+
 // import { NextRequest } from 'next/server';
 
 // const isProtectedRoute = createRouteMatcher(['/service(.*)']);
 
-export default clerkMiddleware();
+// export default clerkMiddleware();
 // export default clerkMiddleware(async () => {
     // Temporarily commented out for development
     // if (isProtectedRoute(req)) await auth.protect();
 // });
+
+export default clerkMiddleware((_auth, _req) => {
+  return NextResponse.next();
+});
 export const config = {
     matcher: [
         // Skip Next.js internals and all static files, unless found in search params
