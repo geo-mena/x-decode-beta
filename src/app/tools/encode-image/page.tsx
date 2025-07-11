2fee3a71dc5199b1a465ac1618d9319c248d57c5
Geovanni Mena <geomenacontact@gmail.com>
2025-07-10 23:20:40 -0500

    fix: add margin-bottom to flex container in multiple page components

--- src/app/tools/encode-image/page.tsx
+++ src/app/tools/encode-image/page.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 export default function page() {
     return (
         <PageContainer scrollable={false}>
-            <div className='flex flex-1 flex-col space-y-4'>
+            <div className='flex flex-1 flex-col space-y-4 mb-4'>
                 <EncodeImage />
             </div>
         </PageContainer>
